68fa24c5622e4c3e1949e1af9328f4eb613bd544
María López <117701355+Majocod@users.noreply.github.com>
2024-03-08 14:53:14 -0500

    Se agrego el modificar datos

--- lib/screens/DataScreen.dart
+++ lib/screens/DataScreen.dart
@@ -3,7 +3,43 @@ import 'package:pilasconelhueco/home/homepage.dart';
 import 'package:pilasconelhueco/shared/styles.dart';
 import '../shared/labels.dart';
 
-class DataScreen extends StatelessWidget {
+class DataScreen extends StatefulWidget {
+  @override
+  _DataScreenState createState() => _DataScreenState();
+}
+
+class _DataScreenState extends State<DataScreen> {
+  // Controladores para los campos de texto
+  TextEditingController nombreController = TextEditingController();
+  TextEditingController celularController = TextEditingController();
+  TextEditingController correoElectronicoController = TextEditingController();
+  TextEditingController generoController = TextEditingController();
+  TextEditingController edadController = TextEditingController();
+
+  bool isEditing = false; // Variable para controlar si se está editando o no
+
+  @override
+  void initState() {
+    // Inicializa los controladores con valores iniciales vacíos
+    nombreController.text = "";
+    celularController.text = "";
+    correoElectronicoController.text = "";
+    generoController.text = "";
+    edadController.text = "";
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Libera los recursos de los controladores cuando se destruye el widget
+    nombreController.dispose();
+    celularController.dispose();
+    correoElectronicoController.dispose();
+    generoController.dispose();
+    edadController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,58 +87,73 @@ class DataScreen extends StatelessWidget {
                 ],
               ),
             ),
-            ListTile(
-              title: Text('Nombre', style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text('Carlos Gutiérrez'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              title: Text('Celular', style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text('+57 3044495884'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              title: Text('Correo electrónico', style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text('carlosgutierrez123@gmail.com'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              title: Text('Genero', style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text('Masculino'),
-              trailing: Icon(Icons.navigate_next),
-            ),
-            ListTile(
-              title: Text('Edad', style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text('40-50'),
-              trailing: Icon(Icons.navigate_next),
-
-            ),
-        Padding(
-          padding: EdgeInsets.only(top: 70.0),
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () {
-              },
-              child: Text(
-                'Guardar',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(ColorsPalet.primaryColor),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40.0),
+            _buildListTile("Nombre", nombreController),
+            _buildListTile("Celular", celularController),
+            _buildListTile("Correo electrónico", correoElectronicoController),
+            _buildListTile("Genero", generoController),
+            _buildListTile("Edad", edadController),
+            Padding(
+              padding: EdgeInsets.only(top: 70.0),
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isEditing = false; // Desactivar edición al guardar
+                    });
+                    // Aquí puedes agregar lógica para guardar los cambios
+                  },
+                  child: Text(
+                    'Guardar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(ColorsPalet.primaryColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListTile(String title, TextEditingController controller) {
+    return ListTile(
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: isEditing
+          ? TextFormField(
+        controller: controller,
+        style: TextStyle(fontSize: 16.0),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey[200],
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.transparent),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: ColorsPalet.primaryColor),
           ),
         ),
-    ],
-    ),
+      )
+          : Text(controller.text),
+      trailing: InkWell(
+        onTap: () {
+          setState(() {
+            isEditing = true;
+          });
+        },
+        child: Icon(Icons.edit),
       ),
     );
   }
